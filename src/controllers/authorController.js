9c30c51903dba12c64b6e3b2aaf296d3c5de01f5
devrajput17 <sdev8484@gmail.com>
2022-05-22 23:38:07 +0530

    populate

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,7 +3,7 @@ const AuthorModel= require("../models/authorModel")
 const createAuthor= async function (req, res) {
     let author = req.body
     let authorCreated = await AuthorModel.create(author)
-    res.send({data: authorCreated})
+    return res.send({data: authorCreated})
 }
 
 const getAuthorsData= async function (req, res) {
--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -4,17 +4,26 @@ const bookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
     let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
-}
+    let author_Id=req.body.authorId
+    let authorRequest = await authorModel.findById(author_Id)
+    if(authorRequest){
+        let bookCreated = await bookModel.create(book)
+        res.send({data: bookCreated})
+
+    }
+    else{
+        res.send({msg :"author Id is not valid"})
+    }
+} 
+
 
 const getBooksData= async function (req, res) {
-    let books = await bookModel.find()
+    let books = await bookModel.find().populate('author')
     res.send({data: books})
 }
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+    let specificBook = await bookModel.find().populate('author_Id')
     res.send({data: specificBook})
 
 }
